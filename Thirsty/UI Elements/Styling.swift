d5cfa74796bdd4920318d5b924e6d2b32db2d862
Stephen Thomas <sthomas@vectorform.com>
2015-03-30 18:17:04 -0400

    First pass at adding fonts and colors.

---
+++ Thirsty/UI Elements/Styling.swift
@@ -0,0 +1,27 @@
+//
+//  Styling.swift
+//  Thirsty
+//
+//  Created by Stephen Thomas on 3/30/15.
+//  Copyright (c) 2015 Final Boss Software. All rights reserved.
+//
+
+import Foundation
+
+extension UIColor {
+    class func backgroundColor () -> UIColor {
+        return UIColor(red: 162, green: 68, blue: 200, alpha: 1)
+    }
+    
+    class func buttonColor () -> UIColor {
+        return UIColor(red: 176, green: 91, blue: 219, alpha: 1)
+    }
+    
+    class func listItemsColor () -> UIColor {
+        return UIColor(red: 186, green: 103, blue: 228, alpha: 1)
+    }
+    
+    class func selectedColor () -> UIColor {
+        return UIColor(red: 109, green: 37, blue: 138, alpha: 1)
+    }
+}
