993109eee06eb7e53130666a7e9b8939e494e529
SLT6723 <stephenthomas@Stephens-Mini.home>
2015-03-31 22:20:09 -0400

    Added external buttons a test photos to the bar detail panel.

--- Thirsty/UI Elements/Buttons.swift
+++ Thirsty/UI Elements/Buttons.swift
@@ -8,17 +8,61 @@
 
 import Foundation
 
-class FavoriteButton: UIControl {
-    let imageView: UIImageView
+class ThirstyButton: UIControl {
+    let label = UILabel()
+    let imageView = UIImageView()
     
     override init() {
-        imageView = UIImageView()
+        super.init()
+        imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        self.addSubview(imageView)
+        
+        label.setTranslatesAutoresizingMaskIntoConstraints(false)
+        label.textColor = UIColor.whiteColor()
+        label.font = UIFont.buttonFont()
+        self.addSubview(label)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+    }
+    
+    required init(coder pDecoder: NSCoder) {
+        super.init(coder: pDecoder)
+    }
+    
+    func touchesBegan() {
+        label.alpha = 0.5
+        imageView.alpha = 0.5
+    }
+    
+    func touchesEnded() {
+        label.alpha = 1
+        imageView.alpha = 1
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        super.touchesBegan(touches, withEvent:  event)
+        touchesBegan()
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        super.touchesEnded(touches, withEvent: event)
+        touchesEnded()
+    }
+    
+    override func touchesCancelled(touches: NSSet!, withEvent event: UIEvent!) {
+        super.touchesCancelled(touches, withEvent: event)
+        touchesEnded()
+    }
+}
+
+class FavoriteButton: ThirstyButton {
+    override init() {
         super.init()
         
         imageView.image = UIImage(named: "HeartEmpty")
-        imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
         imageView.contentMode = UIViewContentMode.Center
-        self.addSubview(imageView)
         
         var constraints: [NSLayoutConstraint] = []
         
@@ -28,12 +72,50 @@ class FavoriteButton: UIControl {
     }
     
     override init(frame: CGRect) {
-        imageView = UIImageView()
         super.init(frame:frame)
     }
     
     required init(coder pDecoder: NSCoder) {
-        imageView = UIImageView()
+        super.init(coder: pDecoder)
+    }
+}
+
+class ExternalButton: ThirstyButton {
+    override init() {
+        super.init()
+        let gapBetweenImageAndLabel = 5
+        
+        let leftSpacer = UIView()
+        leftSpacer.setTranslatesAutoresizingMaskIntoConstraints(false)
+        addSubview(leftSpacer)
+        
+        imageView.contentMode = UIViewContentMode.Center
+        
+        label.textAlignment = .Center
+        
+        let rightSpacer = UIView()
+        rightSpacer.setTranslatesAutoresizingMaskIntoConstraints(false)
+        addSubview(rightSpacer)
+        
+        var constraints: [NSLayoutConstraint] = []
+        
+        constraints += leftSpacer.layoutRelativeTo(self, insets: Inset(0, from: .Top), Inset(0, from: .Left), Inset(0, from: .Bottom))
+        constraints += imageView.layoutRelativeTo(self, insets: Inset(0, from: .Top), Inset(0, from: .Bottom))
+        constraints += imageView.layoutAfter(leftSpacer, distance: 0)
+        constraints += label.layoutRelativeTo(self, insets: Inset(0, from: .Top), Inset(0, from: .Bottom))
+        constraints += label.layoutAfter(imageView, distance: CGFloat(gapBetweenImageAndLabel))
+        constraints += rightSpacer.layoutRelativeTo(self, insets: Inset(0, from: .Top), Inset(0, from: .Right), Inset(0, from: .Bottom))
+        constraints += rightSpacer.constrainWidth(.Equal, toView: leftSpacer, plus: 0)
+        constraints += rightSpacer.layoutAfter(label, distance: 0)
+        
+        addConstraints(constraints)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+    }
+    
+    required init(coder pDecoder: NSCoder) {
         super.init(coder: pDecoder)
     }
 }
--- Thirsty/View Controllers/BarDetail.swift
+++ Thirsty/View Controllers/BarDetail.swift
@@ -26,9 +26,26 @@ class BarDetailView: UIView {
         let topCellView = self.topCellView()
         addSubview(topCellView)
         
+        let externalButtonView = self.externalButtonView()
+        let externalButtonViewHeight = 50
+        addSubview(externalButtonView)
+        
+        let photoAndMapView = self.photoAndMapView()
+        let photoAndMapViewHeight = 234
+        addSubview(photoAndMapView)
+        
         var constraints: [NSLayoutConstraint] = []
         constraints += topCellView.layoutRelativeTo(self, insets: Inset(0, from: .Top), Inset(0, from: .Left), Inset(0, from: .Right))
         constraints += topCellView.constrainHeight(.Equal, toHeight: CGFloat(BarDetailView.topCellHeight))
+        
+        constraints += externalButtonView.layoutRelativeTo(self, insets: Inset(0, from: .Left), Inset(0, from: .Right))
+        constraints += externalButtonView.layoutBelow(topCellView, distance: 0)
+        constraints += externalButtonView.constrainHeight(.Equal, toHeight: CGFloat(externalButtonViewHeight))
+        
+        constraints += photoAndMapView.layoutRelativeTo(self, insets: Inset(0, from: .Left), Inset(0, from: .Right))
+        constraints += photoAndMapView.layoutBelow(externalButtonView, distance: 0)
+        constraints += photoAndMapView.constrainHeight(.Equal, toHeight: CGFloat(photoAndMapViewHeight))
+        
         addConstraints(constraints)
     }
     
@@ -80,5 +97,87 @@ class BarDetailView: UIView {
         topCellView.addConstraints(constraints)
         
         return topCellView
+    }
+    
+    func externalButtonView() -> UIView {
+        let dividerPadding = 5
+        let externalButtonView = UIView()
+        externalButtonView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        externalButtonView.backgroundColor = UIColor.buttonColor()
+        
+        let directionsButton = ExternalButton()
+        directionsButton.label.text = "Directions" //test value
+        directionsButton.imageView.image =  UIImage(named: "DirectionsIcon")
+        directionsButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        externalButtonView.addSubview(directionsButton)
+        
+        let leftDivider = UIView()
+        leftDivider.setTranslatesAutoresizingMaskIntoConstraints(false)
+        leftDivider.backgroundColor = UIColor.backgroundColor()
+        externalButtonView.addSubview(leftDivider)
+        
+        let phoneButton = ExternalButton()
+        phoneButton.label.text = "347-844-0594" //test value
+        phoneButton.imageView.image =  UIImage(named: "PhoneIcon")
+        phoneButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        externalButtonView.addSubview(phoneButton)
+        
+        let rightDivider = UIView()
+        rightDivider.setTranslatesAutoresizingMaskIntoConstraints(false)
+        rightDivider.backgroundColor = UIColor.backgroundColor()
+        externalButtonView.addSubview(rightDivider)
+        
+        let websiteButton = ExternalButton()
+        websiteButton.label.text = "Website" //test value
+        websiteButton.imageView.image =  UIImage(named: "WebIcon")
+        websiteButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        externalButtonView.addSubview(websiteButton)
+        
+        var constraints: [NSLayoutConstraint] = []
+        
+        //constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|[dir][d1(1)][phone][d2(1)][web]|", options: .AlignAllTop, metrics: nil, views: ["dir": directionsButton,"d1": leftDivider, "phone": phoneButton, "d2": rightDivider, "web": websiteButton]) as [NSLayoutConstraint]
+        constraints += directionsButton.layoutRelativeTo(externalButtonView, insets: Inset(0, from: .Left), Inset(0, from: .Top), Inset(0, from: .Bottom))
+        constraints += directionsButton.constrainWidth(.Equal, toView: phoneButton, plus: 0)
+        
+        constraints += leftDivider.layoutAfter(directionsButton, distance: 0)
+        constraints += leftDivider.layoutRelativeTo(externalButtonView, insets: Inset(CGFloat(dividerPadding), from: .Top), Inset(CGFloat(dividerPadding), from: .Bottom))
+        constraints += leftDivider.constrainWidth(.Equal, toWidth: 1)
+        
+        constraints += phoneButton.layoutAfter(leftDivider, distance: 0)
+        constraints += phoneButton.constrainWidth(.Equal, toView: directionsButton, plus: 0)
+        constraints += phoneButton.layoutRelativeTo(externalButtonView, insets: Inset(0, from: .Top), Inset(0, from: .Bottom))
+        
+        constraints += rightDivider.layoutAfter(phoneButton, distance: 0)
+        constraints += rightDivider.layoutRelativeTo(externalButtonView, insets: Inset(CGFloat(dividerPadding), from: .Top), Inset(CGFloat(dividerPadding), from: .Bottom))
+        constraints += rightDivider.constrainWidth(.Equal, toWidth: 1)
+        
+        constraints += websiteButton.layoutAfter(rightDivider, distance: 0)
+        constraints += websiteButton.constrainWidth(.Equal, toView: directionsButton, plus: 0)
+        constraints += websiteButton.layoutRelativeTo(externalButtonView, insets: Inset(0, from: .Right), Inset(0, from: .Top), Inset(0, from: .Bottom))
+        
+        externalButtonView.addConstraints(constraints)
+        
+        return externalButtonView
+    }
+    
+    func photoAndMapView() -> UIView {
+        let photoAndMapView = UIView()
+        photoAndMapView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        photoAndMapView.backgroundColor = UIColor.whiteColor()
+        
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "TestPhotoOne")
+        imageView.contentMode = .ScaleAspectFill
+        imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        imageView.clipsToBounds = true
+        photoAndMapView.addSubview(imageView)
+        
+        var constraints: [NSLayoutConstraint] = []
+        
+        constraints += imageView.layoutInside(photoAndMapView)
+        
+        photoAndMapView.addConstraints(constraints)
+        
+        return photoAndMapView
     }
 }
